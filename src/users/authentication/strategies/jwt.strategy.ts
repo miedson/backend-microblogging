4f7871cf3b3b28cdb77b5f376589e87a004705fa
Miedson Fernandes <miedson.fernandes@hotmail.com>
2024-07-14 01:25:35 -0300

    fix: adjusting for get secret jwt of env

--- src/users/authentication/strategies/jwt.strategy.ts
+++ src/users/authentication/strategies/jwt.strategy.ts
@@ -2,13 +2,15 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from "@nestjs/config";
 import { PayloadJwt } from "../types/payload-jwt.type";
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(private readonly configService: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: 'SecretPasswordForJwt',
+            secretOrKey: configService.get<string>('SECRET_JWT'),
         });
     }
 
